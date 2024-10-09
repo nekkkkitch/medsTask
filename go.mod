a46bbdf4fe8ddca69f3d87c0c12a092007299bc1
JonnykJr1337 <streamerpups@gmail.com>
2024-10-09 21:18:20 +0700

    Finale

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module medstask
 go 1.23.1
 
 require (
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
--- server.go
+++ server.go
@@ -4,61 +4,101 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
 )
 
-var secret_key, _ = rsa.GenerateKey(rand.Reader, 2048)
+var secretKey, _ = rsa.GenerateKey(rand.Reader, 2048)
 
 const dblog = "host=localhost port=5432 user=postgres password=123 dbname=medods sslmode=disable"
 
 func main() {
 	http.HandleFunc("/gettokens", Tokens)
 	http.HandleFunc("/refreshtokens", RefreshTokens)
+	http.HandleFunc("/getrefreshtoken", GetRefreshToken)
 	http.ListenAndServe("localhost:8080", nil)
 }
 
 func Tokens(w http.ResponseWriter, r *http.Request) {
 	guid := r.URL.Query().Get("guid")
-	accessToken, err := jwt.NewWithClaims(jwt.SigningMethodRS512, jwt.RegisteredClaims{
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 5)),
-		Subject:   guid + "/" + r.RemoteAddr,
-	}).SignedString(secret_key)
-	if err != nil {
-		panic(err)
-	}
-	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodRS512, jwt.RegisteredClaims{
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
-		Subject:   guid + "/" + r.RemoteAddr,
-	}).SignedString(secret_key)
-	if err != nil {
-		panic(err)
-	}
+	ip := r.RemoteAddr
+	accessToken, refreshToken := CreateTokens(guid, ip)
 	fmt.Fprintln(w, "Access token: "+accessToken)
 	fmt.Fprintln(w, "Refresh token: "+refreshToken)
 	UpdateUserRefreshToken(guid, refreshToken)
 }
 
+// сверяет совпадение ip внутри payload access токена и ip api запроса, отправляет сообщение в случае несовпадения
+// возвращает новые access и refresh токены и обновляет refresh токен в БД
 func RefreshTokens(w http.ResponseWriter, r *http.Request) {
-
+	accessTokenString := r.URL.Query().Get("accesstoken")
+	claims := jwt.MapClaims{}
+	accessToken, _ := jwt.ParseWithClaims(accessTokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+	fmt.Println(accessToken)
+	guid := strings.Split(claims["sub"].(string), "/")[0]
+	tokenip := strings.Split(claims["sub"].(string), "/")[1]
+	senderip := r.RemoteAddr
+	if tokenip != senderip {
+		fmt.Fprintln(w, "Смена ip!")
+	}
+	newAccessToken, refreshToken := CreateTokens(guid, senderip)
+	fmt.Fprintln(w, "Access token: "+newAccessToken)
+	fmt.Fprintln(w, "Refresh token: "+refreshToken)
+	UpdateUserRefreshToken(guid, refreshToken)
 }
 
+// возвращает refresh token, привязанный к guid пользователя на данный момент(нужно для проверки, что refresh токен вообще меняется)
+func GetRefreshToken(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", dblog)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	guid := r.URL.Query().Get("guid")
+	refreshToken := ""
+	err = db.QueryRow(fmt.Sprintf("select refreshtoken from users where guid='%v'", guid)).Scan(&refreshToken)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprint(w, refreshToken)
+}
+
+// Создаёт access токен с guid и ip в payload, а также бессрочный refresh токен
+func CreateTokens(guid, ip string) (string, string) {
+	accessToken, err := jwt.NewWithClaims(jwt.SigningMethodRS512, jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 5)),
+		Subject:   guid + "/" + ip,
+	}).SignedString(secretKey)
+	if err != nil {
+		panic(err)
+	}
+	refreshToken := uuid.NewString()
+	refreshToken = base64.StdEncoding.EncodeToString([]byte(refreshToken))
+	return accessToken, refreshToken
+}
+
+// Создаёт/обновляет refresh токен, закреплённый в БД за пользователем с данным guid
 func UpdateUserRefreshToken(guid, token string) {
 	db, err := sql.Open("postgres", dblog)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	refreshTokenHash, err := bcrypt.GenerateFromPassword([]byte(token), 10)
+	refreshTokenHash, err := bcrypt.GenerateFromPassword([]byte(token), bcrypt.DefaultCost)
 	if err != nil {
 		panic(err)
 	}
-	_, err = db.Exec("update users set refreshToken=%v where guid=%v", refreshTokenHash, guid)
+	_, err = db.Exec(fmt.Sprintf("update users set refreshtoken='%v' where guid='%v'", refreshTokenHash, guid))
 	if err != nil {
 		panic(err)
 	}
